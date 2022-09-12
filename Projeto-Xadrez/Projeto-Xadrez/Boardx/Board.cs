76cdf698b2d8dd36edd00f69b4595a7c28911508
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 14:58:13 -0300

    Update Board.cs
    cria regra de negocios para o tabuleiro

--- Projeto-Xadrez/Projeto-Xadrez/Boardx/Board.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Boardx/Board.cs
@@ -18,10 +18,42 @@
             return Pieces[line, column];
         }
 
+        public Piece ViewPiece(Position position)
+        {
+            return Pieces[position.Line, position.Column];
+        }
+
+        public bool ExistPiece(Position position)
+        {
+            ValidadePosition(position);
+            return ViewPiece(position) != null;
+        }
+
         public void PutPiece(Piece piece, Position position)
         {
+            if (ExistPiece(position))
+            {
+                throw new BoardException("There is already a piece in that position");
+            }
             Pieces[position.Line, position.Column] = piece;
             piece.Position = position;
+        }
+
+        public bool PositionIsValid(Position position)
+        {
+            if (position.Line < 0 || position.Column < 0 || position.Line > Lines || position.Column > Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidadePosition(Position position)
+        {
+            if (!PositionIsValid(position))
+            {
+                throw new BoardException("Position is not valid");
+            }
         }
     }
 }
