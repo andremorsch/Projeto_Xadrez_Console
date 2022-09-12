a68e0ab295ece365b3486758947b247ab351dc25
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 13:48:44 -0300

    Inicia criacao de pecas
    Inicia criação das peças Torre e Rei no tabuleiro

---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/King.cs
@@ -0,0 +1,18 @@
+﻿using Projeto_Xadrez.Boardx;
+
+namespace Projeto_Xadrez.Chess
+{
+    class King : Piece
+    {
+
+
+        public King(Board board, Color color) : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "K";
+        }
+    }
+}
---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Tower.cs
@@ -0,0 +1,18 @@
+﻿using Projeto_Xadrez.Boardx;
+
+namespace Projeto_Xadrez.Chess
+{
+    class Tower : Piece
+    {
+
+
+        public Tower(Board board, Color color) : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
