016413faf26fcf2d0d624cffcf8c533459a6de3d
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 11:11:17 -0300

    Create Board.cs
    Cria a classe Tabuleiro

---
+++ Projeto-Xadrez/Projeto-Xadrez/Boardx/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Xadrez.Boardx
+{
+    class Board
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+        private Piece[,] Pieces;
+
+        public Board(int lines, int columns)
+        {
+            Lines = lines;
+            Columns = columns;
+            Pieces = new Piece[lines, columns];
+        }
+    }
+}
