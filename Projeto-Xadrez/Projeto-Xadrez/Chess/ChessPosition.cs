0ac14629fb8504e541b1d70df292e2fdff59c460
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 15:09:20 -0300

    Create ChessPosition.cs
    Cria conversor de posições do xadrez para Matriz

---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/ChessPosition.cs
@@ -0,0 +1,29 @@
+﻿using Projeto_Xadrez.Boardx;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Xadrez.Chess
+{
+    class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{Column}{Line}";
+        }
+    }
+}
