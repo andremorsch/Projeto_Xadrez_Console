d482feed0ef53a4650ca5ffe9fcb6b6ba5bbe528
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 13:47:44 -0300

    Update Board.cs
    cria função apare retornar a peça na posição que ela está

--- Projeto-Xadrez/Projeto-Xadrez/Boardx/Board.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Boardx/Board.cs
@@ -17,5 +17,11 @@
         {
             return Pieces[line, column];
         }
+
+        public void PutPiece(Piece piece, Position position)
+        {
+            Pieces[position.Line, position.Column] = piece;
+            piece.Position = position;
+        }
     }
 }
