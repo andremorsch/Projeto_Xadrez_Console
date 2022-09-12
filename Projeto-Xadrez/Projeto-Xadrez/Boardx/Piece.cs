674728537b998a241e0508c18579e03ad0544989
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 11:11:34 -0300

    Create Piece.cs
    Cria classe Peça

---
+++ Projeto-Xadrez/Projeto-Xadrez/Boardx/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Xadrez.Boardx
+{
+    class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int QuantityMoves { get; set; }
+        public Board Board { get; set; }
+
+        public Piece(Position position, Color color, Board board)
+        {
+            Position = position;
+            Color = color;
+            QuantityMoves = 0;
+            Board = board;
+        }
+    }
+}
