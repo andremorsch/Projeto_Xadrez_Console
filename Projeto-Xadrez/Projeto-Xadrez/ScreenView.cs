897cf7ed774235c19491f9fc0c6e2050e7fff3b9
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 16:55:35 -0300

    Update ScreenView.cs
    Diferenciando peças brancas X pretas

--- Projeto-Xadrez/Projeto-Xadrez/ScreenView.cs
+++ Projeto-Xadrez/Projeto-Xadrez/ScreenView.cs
@@ -9,6 +9,7 @@ namespace Projeto_Xadrez
         {
             for (int i = 0; i < board.Lines; i++)
             {
+                Console.Write($"{8 - i} ");
                 for (int j = 0; j < board.Columns; j++)
                 {
                     if (board.ViewPiece(i, j) == null)
@@ -17,10 +18,27 @@ namespace Projeto_Xadrez
                     }
                     else
                     {
-                    Console.Write($"{board.ViewPiece(i, j)} ");
+                        PrintPiece(board.ViewPiece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
