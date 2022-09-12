4cf08f34ec13b9ac06017d36e9e4ea0fe782ffc2
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 11:52:58 -0300

    Create ScreenView.cs
    Cria elemento estático para imprimir o tabuleiro

---
+++ Projeto-Xadrez/Projeto-Xadrez/ScreenView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Projeto_Xadrez.Boardx;
+
+namespace Projeto_Xadrez
+{
+    class ScreenView
+    {
+        public static void PrintBoard(Board board)
+        {
+            for (int i = 0; i < board.Lines; i++)
+            {
+                for (int j = 0; j < board.Columns; j++)
+                {
+                    if (board.ViewPiece(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                    Console.Write($"{board.ViewPiece(i, j)} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
