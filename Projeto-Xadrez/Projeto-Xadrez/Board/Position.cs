d23fb5bbe002885f9437a69b8c9c7b0c5f002445
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-12 10:53:42 -0300

    Create Position.cs
    Cria a classe posição

---
+++ Projeto-Xadrez/Projeto-Xadrez/Board/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_Xadrez.Board
+{
+    class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"{Line}, {Column}";
+        }
+    }
+}
