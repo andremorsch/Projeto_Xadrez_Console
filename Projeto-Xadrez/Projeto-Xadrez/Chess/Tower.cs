ec4b0d347b4fae3720417060891ee468500cb1aa
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-13 14:58:42 -0300

    Update Tower.cs
    Cria regra de negocio para a peça torre

--- Projeto-Xadrez/Projeto-Xadrez/Chess/Tower.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Tower.cs
@@ -14,5 +14,68 @@ namespace Projeto_Xadrez.Chess
         {
             return "T";
         }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.ViewPiece(position);
+            return piece == null || piece.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] moves = new bool[Board.Lines, Board.Columns];
+
+            Position position = new Position(0, 0);
+
+            // up
+            position.SetValues(Position.Line - 1, Position.Column);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Line -= 1;
+            }
+
+            // down
+            position.SetValues(Position.Line + 1, Position.Column);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Line += 1;
+            }
+
+            // right
+            position.SetValues(Position.Line, Position.Column + 1);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Column += 1;
+            }
+
+            // left
+            position.SetValues(Position.Line, Position.Column - 1);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Column -= 1;
+            }
+
+            return moves;
+        }
     }
 }
