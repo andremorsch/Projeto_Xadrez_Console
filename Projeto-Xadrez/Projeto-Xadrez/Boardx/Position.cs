c2ac25483ef82e3d7072ab75593ec80792a4964a
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-13 14:58:54 -0300

    Update Position.cs
    cria metodo para setar valores

--- Projeto-Xadrez/Projeto-Xadrez/Boardx/Position.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Boardx/Position.cs
@@ -11,6 +11,12 @@
             Column = column;
         }
 
+        public void SetValues(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
         public override string ToString()
         {
             return $"{Line}, {Column}";
