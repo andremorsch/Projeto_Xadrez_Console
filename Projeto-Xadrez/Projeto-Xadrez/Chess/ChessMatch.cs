b606303e3578db2d873f7102bc7a9650434d367a
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-13 10:10:14 -0300

    Create ChessMatch.cs
    Cria classe Partida de Xadrez, com modelo de teste com peças

---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/ChessMatch.cs
@@ -0,0 +1,47 @@
+﻿using Projeto_Xadrez.Boardx;
+using System;
+
+namespace Projeto_Xadrez.Chess
+{
+    class ChessMatch
+    {
+        public Board Board { get; private set; }
+        private int Turn;
+        private Color CurrentPlayer;
+        public bool Finished { get; private set; }
+
+        public ChessMatch()
+        {
+            Board = new Board(8, 8);
+            Turn = 1;
+            CurrentPlayer = Color.White;
+            Finished = false;
+            PutPieces();
+        }
+
+        public void MakeMovement(Position origin, Position target)
+        {
+            Piece piece = Board.RemovePiece(origin);
+            piece.IncreaseQuantityMovements();
+            Piece capturedPiece = Board.RemovePiece(target);
+            Board.PutPiece(piece, target);
+        }
+
+        private void PutPieces()
+        {
+            Board.PutPiece(new Tower(Board, Color.White), new ChessPosition('c', 1).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.White), new ChessPosition('c', 2).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.White), new ChessPosition('d', 2).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.White), new ChessPosition('e', 2).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.White), new ChessPosition('e', 1).ToPosition());
+            Board.PutPiece(new King(Board, Color.White), new ChessPosition('d', 1).ToPosition());
+
+            Board.PutPiece(new Tower(Board, Color.Black), new ChessPosition('c', 7).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.Black), new ChessPosition('c', 8).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.Black), new ChessPosition('d', 7).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.Black), new ChessPosition('e', 7).ToPosition());
+            Board.PutPiece(new Tower(Board, Color.Black), new ChessPosition('e', 8).ToPosition());
+            Board.PutPiece(new King(Board, Color.Black), new ChessPosition('d', 8).ToPosition());
+        }
+    }
+}
