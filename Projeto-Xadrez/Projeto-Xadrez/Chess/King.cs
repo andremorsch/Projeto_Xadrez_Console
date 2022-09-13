c4c9a3443cb4c8dec177cc3f8c9e85521fa701b8
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-13 14:58:32 -0300

    Update King.cs
    Cria regra de negocio para a peça Rei

--- Projeto-Xadrez/Projeto-Xadrez/Chess/King.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/King.cs
@@ -14,5 +14,76 @@ namespace Projeto_Xadrez.Chess
         {
             return "K";
         }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.ViewPiece(position);
+            return piece == null || piece.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] moves = new bool[Board.Lines, Board.Columns];
+
+            Position position = new Position(0, 0);
+
+            // up
+            position.SetValues(Position.Line - 1, Position.Column);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            // ne
+            position.SetValues(Position.Line - 1, Position.Column + 1);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            // right
+            position.SetValues(Position.Line, Position.Column + 1);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            // se
+            position.SetValues(Position.Line + 1, Position.Column + 1);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            // down
+            position.SetValues(Position.Line + 1, Position.Column);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            // sw
+            position.SetValues(Position.Line + 1, Position.Column - 1);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            // left
+            position.SetValues(Position.Line, Position.Column - 1);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            // nw
+            position.SetValues(Position.Line - 1, Position.Column - 1);
+            if (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+            }
+
+            return moves;
+        }
     }
 }
