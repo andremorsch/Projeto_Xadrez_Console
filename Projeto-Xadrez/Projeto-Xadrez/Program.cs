36bd546d168e6633bcfa55af55c2e0a2ff1abd6e
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-15 20:36:48 -0300

    Update Program.cs
    programa onde é possivel executar as funções criadas até o momento

--- Projeto-Xadrez/Projeto-Xadrez/Program.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Projeto_Xadrez.Boardx;
+using Projeto_Xadrez.Boardx.Exceptions;
+using Projeto_Xadrez.Chess;
 
 namespace Projeto_Xadrez
 {
@@ -6,7 +9,43 @@ namespace Projeto_Xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                ChessMatch match = new ChessMatch();
+
+                while (!match.Finished)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        ScreenView.PrintMatch(match);
+
+                        Console.Write("\nOrigin position: ");
+                        Position origin = ScreenView.ReadChessPosition().ToPosition();
+                        match.ValidadeOriginPosition(origin);
+
+                        bool[,] PossiblePositions = match.Board.ViewPiece(origin).PossibleMovements();
+
+                        Console.Clear();
+                        ScreenView.PrintBoard(match.Board, PossiblePositions);
+
+                        Console.Write("\nTarget position: ");
+                        Position target = ScreenView.ReadChessPosition().ToPosition();
+                        match.ValidateTargetPosition(origin, target);
+
+                        match.MakeTurn(origin, target);
+                    }
+                    catch (BoardException error)
+                    {
+                        Console.WriteLine(error.Message);
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
