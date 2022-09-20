5abd94030bdf82aa24f2bc2f1f7be7f60bce8745
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-20 09:23:11 -0300

    Create Bishop.cs
    Cria classe do Bispo

---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Bishop.cs
@@ -0,0 +1,21 @@
+﻿using Projeto_Xadrez.Boardx;
+
+namespace Projeto_Xadrez.Chess
+{
+    class Bishop : Piece
+    {
+        public Bishop(Board board, Color color) : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.ViewPiece(position);
+            return piece == null || piece.Color != Color;
+        }
+
