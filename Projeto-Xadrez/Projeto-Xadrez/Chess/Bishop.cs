d4f37ab505a099171f7b25c798e429ca1f9a60cb
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-20 09:23:31 -0300

    Update Bishop.cs
    Cria logica de movimentos do bispo

--- Projeto-Xadrez/Projeto-Xadrez/Chess/Bishop.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Bishop.cs
@@ -19,3 +19,65 @@ namespace Projeto_Xadrez.Chess
             return piece == null || piece.Color != Color;
         }
 
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] moves = new bool[Board.Lines, Board.Columns];
+
+            Position position = new Position(0, 0);
+
+            // NW
+            position.SetValues(Position.Line - 1, Position.Column - 1);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+
+                position.SetValues(position.Line - 1, position.Column - 1);
+            }
+
+            // NE
+            position.SetValues(Position.Line - 1, Position.Column + 1);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+
+                position.SetValues(position.Line - 1, position.Column + 1);
+            }
+
+            // SE
+            position.SetValues(Position.Line + 1, Position.Column + 1);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+
+                position.SetValues(position.Line - 1, position.Column - 1);
+            }
+
+            // SW
+            position.SetValues(Position.Line + 1, Position.Column - 1);
+            while (Board.PositionIsValid(position) && CanMove(position))
+            {
+                moves[position.Line, position.Column] = true;
+                if (Board.ViewPiece(position) != null && Board.ViewPiece(position).Color != Color)
+                {
+                    break;
+                }
+
+                position.SetValues(position.Line + 1, position.Column - 1);
+            }
+              
+            return moves;
+        }
+    }
+}
