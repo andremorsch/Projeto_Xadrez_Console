3dbd0149b65cb14b9704721a836b1b65459733f3
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-20 09:23:46 -0300

    Create Knight.cs
    Cria peça Cavalo

---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Knight.cs
@@ -0,0 +1,21 @@
+﻿using Projeto_Xadrez.Boardx;
+
+namespace Projeto_Xadrez.Chess
+{
+    class Knight : Piece
+    {
+        public Knight(Board board, Color color) : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "N";
+        }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.ViewPiece(position);
+            return piece == null || piece.Color != Color;
+        }
+
