b0c551ecc8a361f368a888c9e2e744dfe38ee5b5
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-20 09:24:12 -0300

    Create Pawn.cs
    cria a peça Peão

---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Pawn.cs
@@ -0,0 +1,21 @@
+﻿using Projeto_Xadrez.Boardx;
+
+namespace Projeto_Xadrez.Chess
+{
+    class Pawn : Piece
+    {
+        public Pawn(Board board, Color color) : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.ViewPiece(position);
+            return piece == null || piece.Color != Color;
+        }
+
