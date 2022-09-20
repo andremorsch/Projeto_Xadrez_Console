62c0f21cbe80f056b968585e791e7ace1eed2571
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-20 10:01:43 -0300

    Update ChessMatch.cs
    Retira código desnecessário

--- Projeto-Xadrez/Projeto-Xadrez/Chess/ChessMatch.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/ChessMatch.cs
@@ -235,19 +235,39 @@ namespace Projeto_Xadrez.Chess
 
         private void PutPieces()
         {
-            PutNewPiece('c', 1, new Rook(Board, Color.White));
-            PutNewPiece('h', 7, new Rook(Board, Color.White));
-            //PutNewPiece('d', 2, new Rook(Board, Color.White));
-            //PutNewPiece('e', 2, new Rook(Board, Color.White));
-            //PutNewPiece('e', 1, new Rook(Board, Color.White));
-            PutNewPiece('d', 1, new King(Board, Color.White));
+            PutNewPiece('a', 1, new Rook(Board, Color.White));
+            PutNewPiece('b', 1, new Knight(Board, Color.White));
+            PutNewPiece('c', 1, new Bishop(Board, Color.White));
+            PutNewPiece('d', 1, new Queen(Board, Color.White));
+            PutNewPiece('e', 1, new King(Board, Color.White));
+            PutNewPiece('f', 1, new Bishop(Board, Color.White));
+            PutNewPiece('g', 1, new Knight(Board, Color.White));
+            PutNewPiece('h', 1, new Rook(Board, Color.White));
+            PutNewPiece('a', 2, new Pawn(Board, Color.White));
+            PutNewPiece('b', 2, new Pawn(Board, Color.White));
+            PutNewPiece('c', 2, new Pawn(Board, Color.White));
+            PutNewPiece('d', 2, new Pawn(Board, Color.White));
+            PutNewPiece('e', 2, new Pawn(Board, Color.White));
+            PutNewPiece('f', 2, new Pawn(Board, Color.White));
+            PutNewPiece('g', 2, new Pawn(Board, Color.White));
+            PutNewPiece('h', 2, new Pawn(Board, Color.White));
 
-            PutNewPiece('b', 8, new Rook(Board, Color.Black));
-            //PutNewPiece('c', 8, new Rook(Board, Color.Black));
-            //PutNewPiece('d', 7, new Rook(Board, Color.Black));
-            //PutNewPiece('e', 7, new Rook(Board, Color.Black));
-            //PutNewPiece('e', 8, new Rook(Board, Color.Black));
-            PutNewPiece('a', 8, new King(Board, Color.Black));
+            PutNewPiece('a', 8, new Rook(Board, Color.Black));
+            PutNewPiece('b', 8, new Knight(Board, Color.Black));
+            PutNewPiece('c', 8, new Bishop(Board, Color.Black));
+            PutNewPiece('d', 8, new Queen(Board, Color.Black));
+            PutNewPiece('e', 8, new King(Board, Color.Black));
+            PutNewPiece('f', 8, new Bishop(Board, Color.Black));
+            PutNewPiece('g', 8, new Knight(Board, Color.Black));
+            PutNewPiece('h', 8, new Rook(Board, Color.Black));
+            PutNewPiece('a', 7, new Pawn(Board, Color.Black));
+            PutNewPiece('b', 7, new Pawn(Board, Color.Black));
+            PutNewPiece('c', 7, new Pawn(Board, Color.Black));
+            PutNewPiece('d', 7, new Pawn(Board, Color.Black));
+            PutNewPiece('e', 7, new Pawn(Board, Color.Black));
+            PutNewPiece('f', 7, new Pawn(Board, Color.Black));
+            PutNewPiece('g', 7, new Pawn(Board, Color.Black));
+            PutNewPiece('h', 7, new Pawn(Board, Color.Black));
         }
     }
 }
