91b3b1f9af9d727ebd4c00f581c0789dbe1de4f2
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-19 23:03:07 -0300

    Create Queen.cs
    Inicia criação da classe Rainha

---
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Queen.cs
@@ -0,0 +1,21 @@
+﻿using Projeto_Xadrez.Boardx;
+
+namespace Projeto_Xadrez.Chess
+{
+    class Queen : Piece
+    {
+        public Queen(Board board, Color color) : base(color, board)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Q";
+        }
+
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.ViewPiece(position);
+            return piece == null || piece.Color != Color;
+        }
+
