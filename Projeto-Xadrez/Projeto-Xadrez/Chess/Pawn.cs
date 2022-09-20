79ab9986871ddc1d575ca0d9588d15ddb6edda9e
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-20 09:25:35 -0300

    Update Pawn.cs
    Logica de movimento do peão

--- Projeto-Xadrez/Projeto-Xadrez/Chess/Pawn.cs
+++ Projeto-Xadrez/Projeto-Xadrez/Chess/Pawn.cs
@@ -30,3 +30,66 @@ namespace Projeto_Xadrez.Chess
             return Board.ViewPiece(position) == null;
         }
 
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] moves = new bool[Board.Lines, Board.Columns];
+
+            Position position = new Position(0, 0);
+
+            if (Color == Color.White)
+            {
+                position.SetValues(Position.Line - 1, Position.Column);
+                if (Board.PositionIsValid(position) && IsEmptySquare(position))
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+
+                position.SetValues(Position.Line - 2, Position.Column);
+                if (Board.PositionIsValid(position) && IsEmptySquare(position) && QuantityMoves == 0)
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+
+                position.SetValues(Position.Line - 1, Position.Column + 1);
+                if (Board.PositionIsValid(position) && HasOpponent(position))
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+
+                position.SetValues(Position.Line - 1, Position.Column - 1);
+                if (Board.PositionIsValid(position) && HasOpponent(position))
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+            }
+            else
+            {
+                position.SetValues(Position.Line + 1, Position.Column);
+                if (Board.PositionIsValid(position) && IsEmptySquare(position))
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+
+                position.SetValues(Position.Line + 2, Position.Column);
+                if (Board.PositionIsValid(position) && IsEmptySquare(position) && QuantityMoves == 0)
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+
+                position.SetValues(Position.Line + 1, Position.Column + 1);
+                if (Board.PositionIsValid(position) && HasOpponent(position))
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+
+                position.SetValues(Position.Line + 1, Position.Column - 1);
+                if (Board.PositionIsValid(position) && HasOpponent(position))
+                {
+                    moves[position.Line, position.Column] = true;
+                }
+            }
+
+            return moves;
+        }
+    }
+}
